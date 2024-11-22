35838f9bba9c0ac639d2a824b77c13a1607fdc96
Eunji0301 <forstudy.eunji@gmail.com>
2024-11-22 16:48:20 +0900

    vscodeConnectandroid

---
+++ myReactNativeApp2/App.web.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {View,Text} from 'react-native'
+
+function App(){
+    return(
+        <View>
+            <Text>
+                Hello World react-native !
+            </Text>
+
+        </View>
+    );
+}
+
+export default App;
